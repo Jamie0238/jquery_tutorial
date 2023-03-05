68528a8e3e6ec707534ce0c97c8ccbfd7ed14bcb
jamie0238 <dmswlsla9745@gmail.com>
2023-03-05 11:45:40 +0900

    jquery

---
+++ 17_magnific_popup/app.js
@@ -0,0 +1,62 @@
+$(function () {
+  //modal
+  $('.popup-modal').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#username',
+    modal: true,
+  })
+  $(document).on('click', '.popup-modal-dismiss', function (e) {
+    e.preventDefault()
+    $.magnificPopup.close()
+  })
+
+  //youtube
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false,
+  })
+
+  //ajax
+  $('.simple-ajax-popup').magnificPopup({
+    type: 'ajax',
+  })
+
+  //single
+  $('.image-popup-vertical-fit').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    mainClass: 'mfp-img-mobile',
+    image: {
+      verticalFit: true,
+    },
+  })
+
+  $('.image-popup-fit-width').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    image: {
+      verticalFit: false,
+    },
+  })
+
+  $('.image-popup-no-margins').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+    image: {
+      verticalFit: true,
+    },
+    zoom: {
+      enabled: true,
+      duration: 300, // don't foget to change the duration also in CSS
+    },
+  })
+})
